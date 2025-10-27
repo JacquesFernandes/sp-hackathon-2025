b3d1a1d645aff4e0b7de57ee53c467415fc7dfe7
JacquesFernandes <jfernandes@scottishpower.com>
2025-10-27 10:58:40 +0000

    feat: Update main search results page to show personalisation sections

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,21 +4,44 @@ import {getDataFromServerSearchProps} from "@/lib/get-data-from-server-search-pr
 import {ServerSearchProps} from "@/lib/types/server-search-props";
 import {SearchInput} from "@/lib/components/search-input";
 import {SearchButton} from "@/lib/components/search-button";
+import {SearchPersonalisation} from "@/lib/components/search-personalisation";
+import {SearchSummaryRag} from "@/lib/components/search-summary-rag";
+import {SearchLinks} from "@/lib/components/search-links";
+import {SearchLink} from "@/lib/types/search-link";
 
 type SearchPageProps = ServerSearchProps;
 
 export default async function SearchPage(props: SearchPageProps) {
   const { search, persona } = await getDataFromServerSearchProps(props);
 
+  const searchPersonalisationContent = "Proin at augue vel odio interdum lobortis at at sapien. Nam a gravida leo, eget pretium urna. Nunc auctor auctor metus quis accumsan. Quisque quis facilisis risus. Suspendisse lobortis tincidunt fermentum. Aenean id neque vel dui imperdiet mollis congue at diam. Sed at aliquet sem.";
+
+  const searchSummaryRagContent = "Proin at augue vel odio interdum lobortis at at sapien. Nam a gravida leo, eget pretium urna. Nunc auctor auctor metus quis accumsan. Quisque quis facilisis risus. Suspendisse lobortis tincidunt fermentum. Aenean id neque vel dui imperdiet mollis congue at diam. Sed at aliquet sem.";
+
+  const links: SearchLink[] = [
+    {
+      label: "ScottishPower",
+      url: "https://www.scottishpower.co.uk/search",
+    },
+    {
+      label: "Google",
+      url: "https://www.google.com/search",
+    }
+  ];
+
   return <PageLayout>
-    <form className="w-full" method="GET" action="/search" >
-      <div className="w-full flex flex-row gap-2" >
-        <SearchInput defaultValue={search} />
-        <input type="hidden" name="persona" value={persona} />
-        <SearchButton>
-          <Search />
-        </SearchButton>
-      </div>
+    <form className="w-full flex flex-row gap-2" method="GET" action="/search"  >
+      <SearchInput defaultValue={search} />
+      <input type="hidden" name="persona" value={persona} />
+      <SearchButton>
+        <Search />
+      </SearchButton>
     </form>
+
+    <SearchPersonalisation content={searchPersonalisationContent} />
+
+    <SearchSummaryRag content={searchSummaryRagContent} />
+
+    <SearchLinks links={links} />
   </PageLayout>;
 }
