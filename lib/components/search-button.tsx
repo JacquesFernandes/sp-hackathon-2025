d33908d249e87084d670128410f9618eb6137b26
JacquesFernandes <jfernandes@scottishpower.com>
2025-10-27 09:43:28 +0000

    fix: add duration for animations in search components

--- lib/components/search-button.tsx
+++ lib/components/search-button.tsx
@@ -4,6 +4,6 @@ export type SearchButtonProps = {
   children: ReactNode;
 };
 
-export const SearchButton: FC<SearchButtonProps> = ({ children }) => <button type="submit" className="rounded-full border border-blue-800 hover:bg-blue-50 p-1 h-[45px] aspect-square flex justify-center items-center cursor-pointer" >
+export const SearchButton: FC<SearchButtonProps> = ({ children }) => <button type="submit" className="rounded-full border border-blue-800 bg-transparent hover:bg-blue-100 transition-colors duration-100 p-1 h-[45px] aspect-square flex justify-center items-center cursor-pointer" >
   {children}
 </button>;
--- lib/components/search-input.tsx
+++ lib/components/search-input.tsx
@@ -8,6 +8,6 @@ export const SearchInput: FC<SearchInputProps> = ({ defaultValue }) => <input
   type="text"
   name="search"
   placeholder="Search for keywords, e.g. ‘Smart meters’"
-  className="w-full outline-1 outline-blue-800 focus:bg-blue-100 rounded-full p-2 px-4 text-xl"
+  className="w-full outline-1 outline-blue-800 bg-transparent focus:bg-blue-100 transition-colors duration-100 rounded-full p-2 px-4 text-xl"
   defaultValue={defaultValue}
 />;
